e1d478d28d2e2886493b27968308e27796414e1d
ChangHyunKim <hot9998@naver.com>
2021-11-16 23:57:20 +0900

    Add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const contact = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <div className="flex justify-center">Contact</div>
+    </>
+  );
+};
+
+export default contact;
