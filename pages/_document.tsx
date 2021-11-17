2f84bb67d410de9c68a2e8b7007dcb24434c87be
ChangHyunKim <hot9998@naver.com>
2021-11-17 23:32:19 +0900

    Add Noto Sans KR font family with google fonts CDN

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
