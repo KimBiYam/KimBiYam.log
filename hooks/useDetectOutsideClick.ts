9ef6ef767a32a95fca6190555cb930160e51b7e0
ChangHyunKim <hot9998@naver.com>
2021-11-19 00:36:55 +0900

    Add useDetectOutsideClick hook

---
+++ hooks/useDetectOutsideClick.ts
@@ -0,0 +1,28 @@
+import { RefObject, useEffect, useState } from "react";
+
+const useDetectOutsideClick = (
+  ref: RefObject<HTMLElement>,
+  initialState = false
+) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const onClick = (event: MouseEvent) => {
+    if (ref.current !== null && !ref.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener("click", onClick);
+    }
+
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, [isOpen, ref]);
+
+  return [isOpen, setIsOpen] as const;
+};
+
+export default useDetectOutsideClick;
