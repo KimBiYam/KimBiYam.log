b879be66990201b67e4c7e55199a8b0c931ff5dc
ChangHyunKim <hot9998@naver.com>
2021-11-22 00:24:40 +0900

    Change header menu item hover color

--- components/base/HeaderMenuItem.tsx
+++ components/base/HeaderMenuItem.tsx
@@ -11,7 +11,9 @@ const HeaderMenuItem = ({ href, label, onClick }: HeaderMenuItemProps) => {
   return (
     <li onClick={onClick}>
       <Link href={href}>
-        <a className="flex text-sm font-bold py-2 hover:bg-gray-300">{label}</a>
+        <a className="flex text-sm font-bold px-1 py-2 my-1 rounded-md hover:bg-white dark:hover:bg-warmGray-800 transition-colors">
+          {label}
+        </a>
       </Link>
     </li>
   );
