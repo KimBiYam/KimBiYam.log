3ce5b79d70ff87f65349319f7a89d4c245ff06c0
ChangHyunKim <hot9998@naver.com>
2021-11-22 23:55:57 +0900

    Change main layout spacing

--- components/base/MainLayout.tsx
+++ components/base/MainLayout.tsx
@@ -7,7 +7,7 @@ export type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="w-full h-full bg-white dark:bg-trueGray-800 transition-colors duration-300">
-      <div className="container h-full max-w-screen-md px-16 pt-16">
+      <div className="container h-full max-w-screen-md px-4 pt-16 md:px-16">
         {children}
       </div>
     </div>
