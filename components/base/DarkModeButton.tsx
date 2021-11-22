de5674a356da26f8190cf174709de7a8b0f9bbba
ChangHyunKim <hot9998@naver.com>
2021-11-22 23:43:46 +0900

    Implement dark mode button animation

--- components/base/DarkModeButton.tsx
+++ components/base/DarkModeButton.tsx
@@ -1,16 +1,35 @@
 import { memo } from "react";
 import MoonIcon from "../../assets/svgs/moon.svg";
+import SunIcon from "../../assets/svgs/sun.svg";
 import useThemeStorage from "../../hooks/useThemeStorage";
+import { Theme } from "../../lib/styles/theme";
 
 export type DarkModeButtonProps = {};
 
 const DarkModeButton = () => {
-  const { toggleTheme } = useThemeStorage();
+  const { theme, toggleTheme } = useThemeStorage();
 
   return (
-    <button type="button" className="flex w-6" onClick={toggleTheme}>
-      <MoonIcon />
-    </button>
+    <label
+      className="relative flex justify-center items-center cursor-pointer"
+      onChange={toggleTheme}
+    >
+      <input
+        type="checkbox"
+        className="peer sr-only"
+        checked={theme === Theme.dark}
+        readOnly
+      />
+      <div className="flex justify-between items-center w-14 h-6 bg-black rounded-full">
+        <div className="flex-1 p-1 text-violet-400">
+          <MoonIcon />
+        </div>
+        <div className="flex-1 p-1 text-yellow-400">
+          <SunIcon />
+        </div>
+      </div>
+      <div className="w-5 h-5 absolute top-2/4 -translate-y-1/2 left-1 bg-white rounded-full peer-checked:translate-x-7 transition-transform duration-300" />
+    </label>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
+  mode: "jit",
   purge: {
     enable: true,
     preserveHtmlElements: false,
