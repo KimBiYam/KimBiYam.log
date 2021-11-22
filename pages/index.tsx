4f66b98b8b9af61dd3650396109e019fd3801de7
ChangHyunKim <hot9998@naver.com>
2021-11-23 00:07:00 +0900

    Fix index page's layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ const Home = () => {
       <Head>
         <title>Main</title>
       </Head>
-      <div className="flex justify-center">
-        <div className="bg-gray-400 py-2 px-8 rounded-md ">
+      <div className="flex flex-col justify-center items-center">
+        <div className="max-w-xs bg-gray-400 py-2 px-8 rounded-md ">
           <p className="text-white">안녕하세요 김창현입니다</p>
         </div>
       </div>
