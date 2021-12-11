0b375b1ca14350ba5ce43167c8c867ed3913f62e
ChangHyunKim <hot9998@naver.com>
2021-12-11 19:30:55 +0900

    Apply prism js theme
    Reference : https://github.com/PrismJS/prism-themes/blob/master/themes/prism-nord.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../lib/styles/globals.css";
+import "../lib/styles/code.css";
 import type { AppProps } from "next/app";
 import Header from "../components/base/Header";
 import Head from "next/head";
