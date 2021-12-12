cacb7fbd9589e71cee6771022bf11186a324cc77
ChangHyunKim <hot9998@naver.com>
2021-12-12 20:26:13 +0900

    Extract google analytics page view to custom hook

---
+++ hooks/useGoogleAnalyticsPageView.ts
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as ga from "../lib/ga";
+
+const useGoogleAnalyticsPageView = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageView(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+};
+
+export default useGoogleAnalyticsPageView;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,10 @@ import "../lib/styles/code.css";
 import type { AppProps } from "next/app";
 import Header from "../components/base/Header";
 import MainLayout from "../components/base/MainLayout";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as ga from "../lib/ga";
+import useGoogleAnalyticsPageView from "../hooks/useGoogleAnalyticsPageView";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      ga.pageView(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  useGoogleAnalyticsPageView();
 
   return (
     <>
