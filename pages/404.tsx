b38da70990ece16fe3357496937dfca2f3eb1292
ChangHyunKim <hot9998@naver.com>
2021-12-15 01:26:43 +0900

    Add 404 not found page - resolve #7

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import PageHead from "../components/base/PageHead";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <PageHead
+        title="KimBiYam.log | Not Found"
+        description="KimBiYam의 개발 블로그 입니다."
+      />
+      <div className="max-w-xl pt-8 mx-auto text-center">
+        <p className="mt-24 text-6xl md:text-8xl font-bold">404</p>
+        <p className="mt-2 text-3xl md:text-6xl font-bold">Page Not Found</p>
+        <p className="mt-6 text-xl">잘못된 경로로 접근하셨습니다.</p>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
