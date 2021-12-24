b32d2fedbc14eaf1744e9bfecbd3234fc14fd7e2
ChangHyunKim <hot9998@naver.com>
2021-12-24 10:51:35 +0900

    Apply next.js scroll restoration flag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
