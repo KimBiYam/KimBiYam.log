169b77b043c15791bc182e10bc6c5cb03972a2b1
ChangHyunKim <hot9998@naver.com>
2021-12-28 00:41:36 +0900

    Change header item text size

--- components/base/HeaderItem.tsx
+++ components/base/HeaderItem.tsx
@@ -8,7 +8,7 @@ export type HeaderItemProps = {
 const HeaderItem = ({ href, label }: HeaderItemProps) => {
   return (
     <Link href={href}>
-      <a className="block lg:mt-0 mr-4 px-2 py-1 hover:bg-warmGray-300 rounded-md transition-colors">
+      <a className="block text-sm mr-4 px-2 py-1 hover:bg-warmGray-300 rounded-md transition-colors">
         {label}
       </a>
     </Link>
