0f01dae4a492b973a4219372a194a24ce35032cf
ChangHyunKim <hot9998@naver.com>
2021-12-28 01:18:03 +0900

    Add footer - resolve #13

---
+++ components/base/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center pt-24 pb-8 text-sm">
+      <p>
+        <span>© 2021</span>
+        <a
+          href="https://github.com/KimBiYam"
+          className="underline ml-1 font-bold"
+        >
+          KimBiYam
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
