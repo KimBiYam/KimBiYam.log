7041a36662e057f36054113cfc8a936ec179b4e4
ChangHyunKim <hot9998@naver.com>
2021-12-31 00:21:10 +0900

    Add tag badge

--- components/posts/PostView.tsx
+++ components/posts/PostView.tsx
@@ -1,5 +1,6 @@
 import { PostData } from '../../types/post.types';
 import MarkdownView from './MarkdownView';
+import TagBadge from './TagBadge';
 import Utterances from './Utterances';
 
 export type PostViewProps = {
@@ -7,12 +8,15 @@ export type PostViewProps = {
 };
 
 const PostView = ({ postData }: PostViewProps) => {
-  const { title, date, contentHtml } = postData;
+  const { title, date, contentHtml, tag } = postData;
 
   return (
     <article className="mt-8 pb-24">
       <h1 className="font-bold text-2xl md:text-4xl"> {title}</h1>
-      <p className="flex mt-2 mb-4 text-sm">{date}</p>
+      <div className="my-4 flex justify-between items-center">
+        <p className="text-sm">{date}</p>
+        <TagBadge tag={tag.toUpperCase()} />
+      </div>
       <MarkdownView contentHtml={contentHtml} />
       <Utterances />
     </article>
