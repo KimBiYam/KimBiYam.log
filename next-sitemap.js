a6e873bf00ee5ad0684492c864bdcf2d44a3fc4a
ChangHyunKim <hot9998@naver.com>
2022-01-05 02:00:19 +0900

    Change dobule quote to single quote in setting files

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL || "https://kimbiyam.me",
+  siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL || 'https://kimbiyam.me',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
