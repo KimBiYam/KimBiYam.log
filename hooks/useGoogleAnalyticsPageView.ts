57fbc726104ba3e535ef6c4bd1bf4faf4dfddb9b
ChangHyunKim <hot9998@naver.com>
2022-01-14 03:40:01 +0900

    Use google analytics when only production mode

--- hooks/useGoogleAnalyticsPageView.ts
+++ hooks/useGoogleAnalyticsPageView.ts
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { IS_PRODUCTION } from '../constants';
 import * as googleAnalytics from '../lib/googleAnalytics';
 
 const useGoogleAnalyticsPageView = () => {
   const router = useRouter();
 
   useEffect(() => {
+    if (!IS_PRODUCTION) {
+      return;
+    }
+
     const handleRouteChange = (url: URL) => {
       googleAnalytics.pageView(url);
     };
