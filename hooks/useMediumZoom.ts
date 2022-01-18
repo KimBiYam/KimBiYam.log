870ea7bb52133d5fd8332d28ada97e588eb0a63d
ChangHyunKim <hot9998@naver.com>
2022-01-19 00:34:45 +0900

    Change medium zoom initial background

--- hooks/useMediumZoom.ts
+++ hooks/useMediumZoom.ts
@@ -1,5 +1,5 @@
 import mediumZoom, { Zoom } from 'medium-zoom';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Theme } from '../constants';
 import colors from '../lib/styles/colors';
 import useThemeStorage from './useThemeStorage';
@@ -8,15 +8,18 @@ const useMediumZoom = (ref: React.RefObject<HTMLElement>) => {
   const { theme } = useThemeStorage();
   const [zoom, setZoom] = useState<Zoom | null>(null);
 
+  const background = useMemo(
+    () => (theme === Theme.dark ? colors.trueGray800 : colors.white),
+    [theme],
+  );
+
   useEffect(() => {
     const images = ref.current?.querySelectorAll('img');
-    setZoom(mediumZoom(images));
+    setZoom(mediumZoom(images, { background }));
   }, []);
 
   useEffect(() => {
-    zoom?.update({
-      background: theme === Theme.dark ? colors.trueGray800 : colors.white,
-    });
+    zoom?.update({ background });
   }, [theme]);
 };
 
