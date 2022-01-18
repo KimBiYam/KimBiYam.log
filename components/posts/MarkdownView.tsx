93f91376340bc7f2bb48bc1a7adf1e23d1e67565
ChangHyunKim <hot9998@naver.com>
2022-01-19 00:04:25 +0900

    Apply medium zoom to markdown images - resolve #8

--- components/posts/MarkdownView.tsx
+++ components/posts/MarkdownView.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useRef } from 'react';
+import mediumZoom from 'medium-zoom';
+
 export type MarkdownViewProps = {
   contentHtml: string;
 };
 
 const MarkdownView = ({ contentHtml }: MarkdownViewProps) => {
+  const markdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const images = markdownRef.current?.querySelectorAll('img');
+    mediumZoom(images);
+  }, []);
+
   return (
     <div className="flex justify-center">
       <div
         className="w-full prose xs:prose-sm sm:prose-sm dark:prose-dark"
+        ref={markdownRef}
         dangerouslySetInnerHTML={{ __html: contentHtml }}
       />
     </div>
