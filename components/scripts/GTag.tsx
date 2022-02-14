75e65bc48a84251b187d928cc48f9a126d732680
ChangHyunKim <hot9998@naver.com>
2022-02-14 23:06:12 +0900

    Extract scripts to component

---
+++ components/scripts/GTag.tsx
@@ -0,0 +1,20 @@
+import Script from 'next/script';
+
+const GTag = () => {
+  return (
+    <Script
+      dangerouslySetInnerHTML={{
+        __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+page_path: window.location.pathname,
+});
+`,
+      }}
+    />
+  );
+};
+
+export default GTag;
