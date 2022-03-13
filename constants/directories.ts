bfcbe024d5d9708748e646977ba37723eaf5633c
ChangHyunKim <hot9998@naver.com>
2022-03-13 18:27:21 +0900

    Rename introduce page to contact page

--- constants/directories.ts
+++ constants/directories.ts
@@ -1,9 +1,9 @@
 import path from 'path';
 
 export const POST_DIRECTORY = path.join(process.cwd(), 'contents', 'posts');
-export const INTRODUCE_DIRECTORY = path.join(
+export const CONTACT_DIRECTORY = path.join(
   process.cwd(),
   'contents',
-  'introduce',
+  'contact',
 );
-export const INTRODUCE_FILE_NAME = 'introduce';
+export const CONTACT_FILE_NAME = 'contact';
