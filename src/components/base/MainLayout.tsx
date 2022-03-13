a6efdaa0cd8251a5b85e1a86e451b9596c114019
ChangHyunKim <hot9998@naver.com>
2022-03-14 01:39:39 +0900

    Change main layout tag

--- src/components/base/MainLayout.tsx
+++ src/components/base/MainLayout.tsx
@@ -5,11 +5,11 @@ export type MainLayoutProps = {
 };
 
 const MainLayout = ({ children }: MainLayoutProps) => (
-  <div className="w-full min-h-[calc(100vh-theme(spacing.8))]">
+  <main className="w-full min-h-[calc(100vh-theme(spacing.8))]">
     <div className="container h-full max-w-screen-md px-8 pt-16">
       {children}
     </div>
-  </div>
+  </main>
 );
 
 export default MainLayout;
