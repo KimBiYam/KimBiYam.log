928608660122898ac123ad5a3e5f08927b244aa2
ChangHyunKim <hot9998@naver.com>
2022-03-15 01:57:51 +0900

    Implement scroll to top with polyfill

--- src/components/base/ScrollToTopButton.tsx
+++ src/components/base/ScrollToTopButton.tsx
@@ -1,5 +1,7 @@
 import { useRecoilValue } from 'recoil';
 import { motion } from 'framer-motion';
+import smoothscroll from 'smoothscroll-polyfill';
+import { useEffect } from 'react';
 import ArrowUpIcon from '../../assets/svgs/arrow_up.svg';
 import scrollUpState from '../../atoms/scrollUpState';
 import { createDynamicallyOpacityMotion } from '../../lib/styles/motions';
@@ -9,6 +11,17 @@ const VISIBLE_TRANSITION_MS = 250;
 const ScrollToTopButton = () => {
   const isScrollUp = useRecoilValue(scrollUpState);
 
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <motion.div
       className="fixed z-50 right-4 bottom-12 md:right-8 lg:right-16"
@@ -16,6 +29,7 @@ const ScrollToTopButton = () => {
     >
       <button
         type="button"
+        onClick={handleClick}
         className="flex items-center justify-center w-10 h-10 rounded-full drop-shadow-md bg-blueGray-200 justify-items-center dark:bg-gray-700"
       >
         <div className="w-4 h-4 main-font-color">
