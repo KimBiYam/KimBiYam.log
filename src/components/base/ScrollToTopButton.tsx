7dfd7dbc0f9278e8e5f7e454d91a044e8ce48a4b
ChangHyunKim <hot9998@naver.com>
2022-03-15 01:43:28 +0900

    Apply motion to ScrollToTopButton component

--- src/components/base/ScrollToTopButton.tsx
+++ src/components/base/ScrollToTopButton.tsx
@@ -1,14 +1,29 @@
+import { useRecoilValue } from 'recoil';
+import { motion } from 'framer-motion';
 import ArrowUpIcon from '../../assets/svgs/arrow_up.svg';
+import scrollUpState from '../../atoms/scrollUpState';
+import { createDynamicallyOpacityMotion } from '../../lib/styles/motions';
 
-const ScrollToTopButton = () => (
-  <button
-    type="button"
-    className="fixed flex items-center justify-center w-10 h-10 rounded-full bg-blueGray-200 justify-items-center right-12 bottom-12 dark:bg-gray-700"
-  >
-    <div className="w-4 h-4 main-font-color">
-      <ArrowUpIcon />
-    </div>
-  </button>
-);
+const VISIBLE_TRANSITION_MS = 250;
+
+const ScrollToTopButton = () => {
+  const isScrollUp = useRecoilValue(scrollUpState);
+
+  return (
+    <motion.div
+      className="fixed z-50 right-36 bottom-12"
+      {...createDynamicallyOpacityMotion(!isScrollUp, VISIBLE_TRANSITION_MS)}
+    >
+      <button
+        type="button"
+        className="flex items-center justify-center w-10 h-10 rounded-full drop-shadow-md bg-blueGray-200 justify-items-center dark:bg-gray-700"
+      >
+        <div className="w-4 h-4 main-font-color">
+          <ArrowUpIcon />
+        </div>
+      </button>
+    </motion.div>
+  );
+};
 
 export default ScrollToTopButton;
