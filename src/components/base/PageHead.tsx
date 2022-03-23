97d1b0f085f63ca2f01839cfa945fbc941b88371
ChangHyunKim <hot9998@naver.com>
2022-03-24 00:41:52 +0900

    Add deafult value to PageHead component's url prop

--- src/components/base/PageHead.tsx
+++ src/components/base/PageHead.tsx
@@ -11,7 +11,12 @@ export type PageHeadProps = {
   ogImagePath?: string;
 };
 
-const PageHead = ({ title, description, url, ogImagePath }: PageHeadProps) => (
+const PageHead = ({
+  title,
+  description,
+  url = '',
+  ogImagePath,
+}: PageHeadProps) => (
   <Head>
     <Favicon />
     <title>{title}</title>
