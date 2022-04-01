ecea276908dca2ed8705c01db66a7f487c0fd4d3
ChangHyunKim <hot9998@naver.com>
2022-04-01 23:53:06 +0900

    Add anchor tag into PostListItem component

--- src/components/posts/PostListItem.tsx
+++ src/components/posts/PostListItem.tsx
@@ -14,21 +14,23 @@ const PostListItem = ({ postPreview }: PostListItemProps) => {
 
   return (
     <NextLink href={`/posts/${id}`}>
-      <motion.div
-        className="py-4 cursor-pointer hover:text-shadow dark:hover:text-shadow-dark transition-text-shadow"
-        {...viewPortOpacityMotion}
-      >
-        <h3 className="w-full overflow-hidden text-2xl font-bold truncate whitespace-nowrap">
-          {title}
-        </h3>
-        <div className="flex items-center justify-between my-1">
-          <p className="text-sm">{date}</p>
-          <TagBadge tag={tag.toUpperCase()} />
-        </div>
-        <p className="overflow-hidden text-sm dark:text-zinc-400 text-zinc-700 md:text-base">
-          {content}
-        </p>
-      </motion.div>
+      <a>
+        <motion.div
+          className="py-4 cursor-pointer hover:text-shadow dark:hover:text-shadow-dark transition-text-shadow"
+          {...viewPortOpacityMotion}
+        >
+          <h3 className="w-full overflow-hidden text-2xl font-bold truncate whitespace-nowrap">
+            {title}
+          </h3>
+          <div className="flex items-center justify-between my-1">
+            <p className="text-sm">{date}</p>
+            <TagBadge tag={tag.toUpperCase()} />
+          </div>
+          <p className="overflow-hidden text-sm dark:text-zinc-400 text-zinc-700 md:text-base">
+            {content}
+          </p>
+        </motion.div>
+      </a>
     </NextLink>
   );
 };
