b65e57246d97f2c1efc6b8687bde899313c9a880
ChangHyunKim <hot9998@naver.com>
2022-04-01 23:55:19 +0900

    Add margin into header nav

--- src/components/base/HeaderDesktopNav.tsx
+++ src/components/base/HeaderDesktopNav.tsx
@@ -1,8 +1,8 @@
 import HeaderItem from './HeaderItem';
 
 const HeaderDesktopNav = () => (
-  <nav className="flex items-center justify-end flex-1">
-    <div className="hidden ml-4 font-bold md:block">
+  <nav className="flex items-center justify-end flex-1 mr-4">
+    <div className="hidden font-bold md:block">
       <HeaderItem href="/contact" label="Contact" />
     </div>
   </nav>
