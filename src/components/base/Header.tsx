52e9bc762b7a006feca0892881d9c2ba40038e0a
ChangHyunKim <hot9998@naver.com>
2022-04-01 23:38:11 +0900

    Extract Logo component

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,15 +1,13 @@
 import { memo } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { useRecoilValue } from 'recoil';
 import HeaderItem from './HeaderItem';
 import HeaderMenu from './HeaderMenu';
 import DarkModeButton from './DarkModeButton';
-import favicon from '../../assets/favicon/favicon-192x192.png';
 import scrollState from '../../atoms/scrollState';
 import { createDynamicallyOpacityMotion } from '../../lib/styles/motions';
 import { ScrollDirection } from '../../constants';
+import Logo from './Logo';
 
 const VISIBLE_TRANSITION_MS = 100;
 
@@ -25,18 +23,7 @@ const Header = () => {
       )}
     >
       <div className="container flex items-center justify-between h-full max-w-screen-md px-4 text-black md:px-8 dark:text-gray-300">
-        <Link href="/" passHref>
-          <button type="button" className="flex p-2">
-            <Image
-              src={favicon}
-              alt="logo"
-              width="24px"
-              height="24px"
-              quality={100}
-              className="cursor-pointer "
-            />
-          </button>
-        </Link>
+        <Logo />
         <nav className="flex items-center justify-end flex-1">
           <div className="hidden ml-4 font-bold md:block">
             <HeaderItem href="/contact" label="Contact" />
