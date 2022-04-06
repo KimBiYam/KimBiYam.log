09f021c5bacc2ed14cee7d69c22b1559d63f7bca
ChangHyunKim <hot9998@naver.com>
2022-04-07 01:25:29 +0900

    Fix header button section's layout

--- src/components/base/HeaderButtonSection.tsx
+++ src/components/base/HeaderButtonSection.tsx
@@ -2,7 +2,7 @@ import DarkModeButton from './DarkModeButton';
 import HeaderMobileMenu from './HeaderMobileMenu';
 
 const HeaderButtonSection = () => (
-  <section className="flex">
+  <section className="flex items-center">
     <DarkModeButton />
     <HeaderMobileMenu />
   </section>
