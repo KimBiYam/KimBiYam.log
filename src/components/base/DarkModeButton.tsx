4203762460ca51e34f42d5509586c29231cc495f
ChangHyunKim <hot9998@naver.com>
2022-04-07 01:44:47 +0900

    Change dark mode button design with animation

--- src/components/base/DarkModeButton.tsx
+++ src/components/base/DarkModeButton.tsx
@@ -4,39 +4,40 @@ import MoonIcon from '../../assets/svgs/moon.svg';
 import SunIcon from '../../assets/svgs/sun.svg';
 import { Theme } from '../../constants';
 import useTheme from '../../hooks/useTheme';
+import { createRotateScaleMotion } from '../../lib/styles/motions';
+
+const BUTTON_TRANSITION_MS = 300;
 
 const DarkModeButton = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div
-      className="relative flex items-center justify-center cursor-pointer"
+    <button
+      type="button"
+      className="p-2 rounded-full cursor-pointer primary-button-hover"
       onClick={toggleTheme}
     >
-      <div className="flex items-center justify-between h-6 bg-black rounded-full w-14">
-        <div className="flex-1 p-1 text-violet-400">
-          <MoonIcon />
-        </div>
-        <div className="flex-1 p-1 text-yellow-400">
-          <SunIcon />
-        </div>
-      </div>
-      <div
-        className={`absolute w-full px-1 flex justify-start ${
-          theme === Theme.dark && 'justify-end'
-        }`}
-      >
+      <div className="relative w-6 h-6">
         <motion.div
-          className="w-5 h-5 bg-white rounded-full"
-          layout
-          transition={{
-            type: 'spring',
-            stiffness: 500,
-            damping: 28,
-          }}
-        />
+          className="absolute w-full"
+          {...createRotateScaleMotion(
+            theme === Theme.dark,
+            BUTTON_TRANSITION_MS,
+          )}
+        >
+          <MoonIcon />
+        </motion.div>
+        <motion.div
+          className="absolute w-full"
+          {...createRotateScaleMotion(
+            theme === Theme.light,
+            BUTTON_TRANSITION_MS,
+          )}
+        >
+          <SunIcon />
+        </motion.div>
       </div>
-    </div>
+    </button>
   );
 };
 
