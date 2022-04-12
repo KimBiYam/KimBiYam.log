c7bd2aafb5e999ebd1e0b29c59f05b292e7ba1b8
ChangHyunKim <hot9998@naver.com>
2022-04-13 00:19:28 +0900

    Edit robots.txt

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL || 'https://kimbiyam.me',
-  generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 1,
