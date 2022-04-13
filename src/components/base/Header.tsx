9ab472b44df298165f8ca28f4593375af48cfc3e
ChangHyunKim <hot9998@naver.com>
2022-04-13 19:49:01 +0900

    Change header display conditional

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -9,17 +9,17 @@ import HeaderDesktopNav from './HeaderDesktopNav';
 import HeaderButtonSection from './HeaderButtonSection';
 
 const VISIBLE_TRANSITION_MS = 100;
+const HEADER_HEIGHT = 32;
 
 const Header = () => {
-  const { direction } = useRecoilValue(scrollState);
+  const { direction, pageY } = useRecoilValue(scrollState);
+
+  const isShow = pageY < HEADER_HEIGHT / 2 || direction === ScrollDirection.up;
 
   return (
     <motion.header
       className="fixed z-50 flex items-center w-full h-14 main-container"
-      {...createDynamicallyOpacityMotion(
-        direction === ScrollDirection.up,
-        VISIBLE_TRANSITION_MS,
-      )}
+      {...createDynamicallyOpacityMotion(isShow, VISIBLE_TRANSITION_MS)}
     >
       <div className="container flex items-center justify-between h-full max-w-screen-md px-4 text-black md:px-8 dark:text-gray-300">
         <Logo />
