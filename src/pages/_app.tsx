8d44fd4d2ca1c39d8b8f3240e5c677ec7d09a5c6
ChangHyunKim <hot9998@naver.com>
2022-05-21 23:57:44 +0900

    Apply AnimatePresence to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../lib/styles/global.css';
 import '../lib/styles/code.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { AnimatePresence } from 'framer-motion';
 import Header from '../components/base/Header';
 import MainLayout from '../components/base/MainLayout';
 import useGoogleAnalyticsPageView from '../hooks/useGoogleAnalyticsPageView';
@@ -12,14 +13,19 @@ import ScrollToTopButton from '../components/base/ScrollToTopButton';
 
 if (IS_PRODUCTION) sentryUtil.init();
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps, router }: AppProps) => {
   useGoogleAnalyticsPageView();
 
   return (
     <RecoilRoot>
       <Header />
       <MainLayout>
-        <Component {...pageProps} />
+        <AnimatePresence
+          exitBeforeEnter
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </MainLayout>
       <Footer />
       <ScrollToTopButton />
