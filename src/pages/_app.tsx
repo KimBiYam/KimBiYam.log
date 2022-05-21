64a3f716703a4fc9498473708e6064de23745275
ChangHyunKim <hot9998@naver.com>
2022-05-22 01:04:21 +0900

    Apply preserve scroll and fix animate exit complete event

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { AnimatePresence } from 'framer-motion';
 import smoothscroll from 'smoothscroll-polyfill';
+import { useEffect } from 'react';
 import Header from '../components/base/Header';
 import MainLayout from '../components/base/MainLayout';
 import useGoogleAnalyticsPageView from '../hooks/useGoogleAnalyticsPageView';
@@ -11,22 +12,35 @@ import * as sentryUtil from '../lib/utils/sentry.util';
 import Footer from '../components/base/Footer';
 import { IS_PRODUCTION } from '../constants';
 import ScrollToTopButton from '../components/base/ScrollToTopButton';
+import { usePreserveScroll } from '../hooks/usePreserveScroll';
 
 if (IS_PRODUCTION) sentryUtil.init();
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   useGoogleAnalyticsPageView();
 
+  const { currentScrollPosition } = usePreserveScroll();
+
   useEffect(() => {
     smoothscroll.polyfill();
   }, []);
+
+  const handleAnimateExitComplete = () => {
+    if (currentScrollPosition !== 0) {
+      window.scrollTo({ top: 0 });
+      window.scrollTo({ top: currentScrollPosition, behavior: 'smooth' });
+    } else {
+      window.scrollTo({ top: 0 });
+    }
+  };
+
   return (
     <RecoilRoot>
       <Header />
       <MainLayout>
         <AnimatePresence
           exitBeforeEnter
-          onExitComplete={() => window.scrollTo(0, 0)}
+          onExitComplete={handleAnimateExitComplete}
         >
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
