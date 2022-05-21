133e64c3203497b7f7b3753ee222d0c03ad44f63
ChangHyunKim <hot9998@naver.com>
2022-05-22 01:02:22 +0900

    Implement usePreserveScroll hook

---
+++ src/hooks/usePreserveScroll.ts
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router';
+import { useEffect, useRef } from 'react';
+
+export const usePreserveScroll = () => {
+  const router = useRouter();
+  const url = router.asPath;
+
+  const scrollPositionsRef = useRef<Record<string, number>>({});
+  const isPopRef = useRef(false);
+
+  const currentScrollPosition = scrollPositionsRef.current[url] ?? 0;
+
+  useEffect(() => {
+    router.beforePopState(() => {
+      isPopRef.current = true;
+      return true;
+    });
+
+    const handleRouteChangeStart = () => {
+      scrollPositionsRef.current[url] = window.scrollY;
+    };
+
+    const handleRouteChangeComplete = () => {
+      isPopRef.current = false;
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router]);
+
+  return { currentScrollPosition };
+};
