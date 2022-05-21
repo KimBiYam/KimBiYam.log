b15b9fee52e647858d2b82f6b36338e894e823ec
ChangHyunKim <hot9998@naver.com>
2022-05-22 00:00:17 +0900

    Off next config's scrollRestoration option

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
