31adafdf02eedb16d69895da0613f450d53373ea
ChangHyunKim <hot9998@naver.com>
2022-05-21 22:40:52 +0900

    Remove post view page's animation

--- src/components/posts/PostView.tsx
+++ src/components/posts/PostView.tsx
@@ -14,20 +14,18 @@ const PostView = ({ postDetail }: PostViewProps) => {
   const { title, date, contentHtml, tag } = postDetail;
 
   return (
-    <motion.div {...opacityMotion}>
-      <article className="pb-24 mt-8">
-        <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
-        <div className="flex items-center justify-between my-4">
-          <p className="text-sm">{date}</p>
-          <TagBadge tag={tag.toUpperCase()} />
-        </div>
-        <MarkdownView contentHtml={contentHtml} />
-        <div className="py-4 my-10 border-t border-b">
-          <ProfileCard />
-        </div>
-        <Utterances />
-      </article>
-    </motion.div>
+    <article className="pb-24 mt-8">
+      <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
+      <div className="flex items-center justify-between my-4">
+        <p className="text-sm">{date}</p>
+        <TagBadge tag={tag.toUpperCase()} />
+      </div>
+      <MarkdownView contentHtml={contentHtml} />
+      <div className="py-4 my-10 border-t border-b">
+        <ProfileCard />
+      </div>
+      <Utterances />
+    </article>
   );
 };
 
