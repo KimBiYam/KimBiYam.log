b47fca5d165b5eeae5034002871bb834f668476a
ChangHyunKim <hot9998@naver.com>
2022-05-22 17:41:45 +0900

    Change tag badge colors

--- src/components/posts/TagBadge.tsx
+++ src/components/posts/TagBadge.tsx
@@ -3,7 +3,7 @@ export type TagBadgeProps = {
 };
 
 const TagBadge = ({ tag }: TagBadgeProps) => (
-  <p className="inline-block px-2 py-1 text-xs font-semibold bg-gray-300 md:text-sm dark:bg-gray-600 rounded-xl">
+  <p className="inline-block px-2 py-1 text-xs font-semibold bg-neutral-300 md:text-sm dark:bg-neutral-600 rounded-xl">
     {tag.toUpperCase()}
   </p>
 );
