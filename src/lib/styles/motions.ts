7ecf0d53acf54cb1353e4bf0ebcec31f021cb6a7
ChangHyunKim <hot9998@naver.com>
2022-05-28 22:28:06 +0900

    Delete unusing motions

--- src/lib/styles/motions.ts
+++ src/lib/styles/motions.ts
@@ -2,21 +2,6 @@ import { MotionProps } from 'framer-motion';
 
 const DEFAULT_DURATION = 0.5;
 
-export const opacityMotion: MotionProps = {
-  animate: { opacity: [0, 1] },
-  transition: { duration: DEFAULT_DURATION },
-};
-
-export const slideUpMotion: MotionProps = {
-  animate: { y: [20, 0], opacity: [0, 1] },
-  transition: { duration: DEFAULT_DURATION },
-};
-
-export const slideLeftMotion: MotionProps = {
-  animate: { x: [20, 0], opacity: [0, 1] },
-  transition: { duration: DEFAULT_DURATION },
-};
-
 export const viewPortOpacityMotion: MotionProps = {
   initial: { opacity: 0 },
   whileInView: { opacity: [0, 1] },
