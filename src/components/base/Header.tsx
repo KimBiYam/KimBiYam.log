2aa219ce7c50f6c49cd5b6b662d99f3e2e4f5b57
ChangHyunKim <hot9998@naver.com>
2022-05-30 20:30:02 +0900

    Change header background color

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
 
   return (
     <motion.header
-      className="fixed z-50 flex items-center w-full h-14 main-container"
+      className="fixed z-50 flex items-center w-full h-14 bg-inherit"
       {...createDynamicallyOpacityMotion(isShown, VISIBLE_TRANSITION_MS)}
     >
       <div className="container flex items-center justify-between h-full max-w-screen-md px-4 md:px-8 ">
