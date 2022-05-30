e2aed17f48696d3f74797bb16c640f60e89d3755
ChangHyunKim <hot9998@naver.com>
2022-05-30 20:50:32 +0900

    Modify post list markup tags

--- src/components/posts/PostListItem.tsx
+++ src/components/posts/PostListItem.tsx
@@ -13,27 +13,25 @@ const PostListItem = ({ postPreview }: PostListItemProps) => {
   const { id, title, date, content, tag } = postPreview;
 
   return (
-    <li>
-      <NoScrollLink href={`/posts/${id}`}>
-        <a>
-          <motion.div
-            className="py-4 cursor-pointer lg:duration-300 lg:hover:-translate-y-2"
-            {...viewportOpacityMotion}
-          >
-            <h3 className="w-full overflow-hidden text-2xl font-bold truncate whitespace-nowrap">
-              {title}
-            </h3>
-            <div className="flex items-center justify-between my-1">
-              <p className="text-sm">{date}</p>
-              <TagBadge tag={tag.toUpperCase()} />
-            </div>
-            <p className="overflow-hidden text-sm dark:text-zinc-400 text-zinc-700 md:text-base">
-              {content}
-            </p>
-          </motion.div>
-        </a>
-      </NoScrollLink>
-    </li>
+    <NoScrollLink href={`/posts/${id}`}>
+      <a>
+        <motion.div
+          className="py-4 cursor-pointer lg:duration-300 lg:hover:-translate-y-2"
+          {...viewportOpacityMotion}
+        >
+          <h3 className="w-full overflow-hidden text-2xl font-bold truncate whitespace-nowrap">
+            {title}
+          </h3>
+          <div className="flex items-center justify-between my-1">
+            <p className="text-sm">{date}</p>
+            <TagBadge tag={tag.toUpperCase()} />
+          </div>
+          <p className="overflow-hidden text-sm dark:text-zinc-400 text-zinc-700 md:text-base">
+            {content}
+          </p>
+        </motion.div>
+      </a>
+    </NoScrollLink>
   );
 };
 
