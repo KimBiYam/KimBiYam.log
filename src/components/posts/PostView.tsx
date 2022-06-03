7e01f24a3436f9e9339aedd9b278b31b272994f9
ChangHyunKim <hot9998@naver.com>
2022-06-03 14:57:00 +0900

    Implement table of contents

--- src/components/posts/PostView.tsx
+++ src/components/posts/PostView.tsx
@@ -1,6 +1,7 @@
 import { PostDetail } from '../../types/post.types';
 import ProfileCard from '../base/ProfileCard';
 import MarkdownView from './MarkdownView';
+import TableOfContents from './TableOfContents';
 import TagBadge from './TagBadge';
 import Utterances from './Utterances';
 
@@ -12,18 +13,21 @@ const PostView = ({ postDetail }: PostViewProps) => {
   const { title, date, contentHtml, tag } = postDetail;
 
   return (
-    <article className="pb-24 mt-8">
-      <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
-      <div className="flex items-center justify-between my-4">
-        <p className="text-sm">{date}</p>
-        <TagBadge tag={tag.toUpperCase()} />
-      </div>
-      <MarkdownView contentHtml={contentHtml} />
-      <div className="py-4 my-10 border-t border-b">
-        <ProfileCard />
-      </div>
-      <Utterances />
-    </article>
+    <>
+      <article className="pb-24 mt-8">
+        <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
+        <div className="flex items-center justify-between my-4">
+          <p className="text-sm">{date}</p>
+          <TagBadge tag={tag.toUpperCase()} />
+        </div>
+        <MarkdownView contentHtml={contentHtml} />
+        <div className="py-4 my-10 border-t border-b">
+          <ProfileCard />
+        </div>
+        <Utterances />
+      </article>
+      <TableOfContents />
+    </>
   );
 };
 
