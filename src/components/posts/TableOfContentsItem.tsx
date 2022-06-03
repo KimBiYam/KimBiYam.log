3c80189c16f4e1af80d6f7da09c47d99c06e0a2c
ChangHyunKim <hot9998@naver.com>
2022-06-04 01:52:24 +0900

    Apply smooth scroll to table of contents

--- src/components/posts/TableOfContentsItem.tsx
+++ src/components/posts/TableOfContentsItem.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { TableOfContentHeading } from '../../hooks/useTableOfContents';
 
 export interface TableOfContentsItemProps {
@@ -11,13 +12,25 @@ const TableOfContentsItem = ({
   children,
   activeId,
 }: TableOfContentsItemProps) => {
+  const router = useRouter();
+
   const { id, title } = heading;
+
+  const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault();
+
+    document
+      .querySelector(`#${CSS.escape(id)}`)
+      ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    router.replace(`#${id}`);
+  };
 
   return (
     <li className="my-1 overflow-hidden text-sm text-ellipsis whitespace-nowrap">
       <a
         href={`#${id}`}
         className={activeId === id ? 'opacity-100 font-bold' : `opacity-70`}
+        onClick={handleClick}
       >
         {title}
       </a>
