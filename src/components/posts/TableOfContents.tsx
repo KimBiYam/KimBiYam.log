bf834df22a25142fc31d7deea99cc145b8984585
ChangHyunKim <hot9998@naver.com>
2022-06-04 12:49:38 +0900

    Implemenet toc auto scroll to active item

--- src/components/posts/TableOfContents.tsx
+++ src/components/posts/TableOfContents.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react';
 import withDragScroll from '../../hocs/withDragScroll';
 import useTableHeadingObserver from '../../hooks/useTableHeadingObserver';
 import useTableOfContents from '../../hooks/useTableOfContents';
@@ -7,6 +7,15 @@ import TableOfContentsItem from './TableOfContentsItem';
 const TableOfContents = forwardRef<HTMLElement>((_: unknown, ref) => {
   const headings = useTableOfContents();
   const activeId = useTableHeadingObserver();
+
+  const itemRefs = useRef<Record<string, HTMLLIElement | null>>({});
+
+  useEffect(() => {
+    if (!ref || typeof ref === 'function' || !ref.current) return;
+
+    const activeItem = itemRefs.current[activeId];
+    ref.current.scrollTo({ top: activeItem?.offsetTop, behavior: 'smooth' });
+  }, [activeId]);
 
   return (
     <nav
@@ -19,6 +28,9 @@ const TableOfContents = forwardRef<HTMLElement>((_: unknown, ref) => {
             key={heading.id}
             heading={heading}
             activeId={activeId}
+            ref={(instance) => {
+              itemRefs.current[heading.id] = instance;
+            }}
           >
             {Array.isArray(heading.items) && heading.items?.length > 0 && (
               <ul className="ml-3">
@@ -27,6 +39,9 @@ const TableOfContents = forwardRef<HTMLElement>((_: unknown, ref) => {
                     key={item.id}
                     heading={item}
                     activeId={activeId}
+                    ref={(instance) => {
+                      itemRefs.current[item.id] = instance;
+                    }}
                   />
                 ))}
               </ul>
