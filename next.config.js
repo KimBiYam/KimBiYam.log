c629421a3034f3e2e9fd5cf6616ff14bc999050e
ChangHyunKim <hot9998@naver.com>
2022-06-05 18:52:29 +0900

    Apply next/image device size
    Reference : https://nextjs.org/docs/api-reference/next/image\#device-sizes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    deviceSizes: [480, 768, 1024, 1280],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
