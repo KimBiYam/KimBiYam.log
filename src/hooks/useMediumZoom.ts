9110d5e3bfeb476c2b0d6ed17aba8e4017e54c01
ChangHyunKim <hot9998@naver.com>
2022-06-05 19:17:08 +0900

    Refactor useMediumZoom hook to use useRef

--- src/hooks/useMediumZoom.ts
+++ src/hooks/useMediumZoom.ts
@@ -1,12 +1,12 @@
 import mediumZoom, { Zoom } from 'medium-zoom';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { Theme } from '../constants';
 import useTheme from './useTheme';
 import { theme as tailwindTheme } from '../../tailwind.config';
 
 const useMediumZoom = (ref: React.RefObject<HTMLElement>) => {
   const { theme } = useTheme();
-  const [zoom, setZoom] = useState<Zoom | null>(null);
+  const zoomRef = useRef<Zoom | null>(null);
 
   const background = useMemo(
     () =>
@@ -18,19 +18,16 @@ const useMediumZoom = (ref: React.RefObject<HTMLElement>) => {
 
   useEffect(() => {
     const images = ref.current?.querySelectorAll('img');
-    setZoom(mediumZoom(images, { background, margin: 24 }));
+    zoomRef.current = mediumZoom(images, { background, margin: 24 });
+
+    return () => {
+      if (zoomRef.current) zoomRef.current.detach();
+    };
   }, []);
 
   useEffect(() => {
-    zoom?.update({ background });
+    zoomRef.current?.update({ background });
   }, [theme]);
-
-  useEffect(
-    () => () => {
-      if (zoom) zoom.close();
-    },
-    [zoom],
-  );
 };
 
 export default useMediumZoom;
