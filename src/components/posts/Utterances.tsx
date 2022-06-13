a6f23b9ffe6a3f688e7099e4963f7ba8269d90d4
ChangHyunKim <hot9998@naver.com>
2022-06-13 20:33:39 +0900

    Implement utterances dynamic theme changes
    Reference : https://github.com/utterance/utterances/issues/401\#issuecomment-705083676

--- src/components/posts/Utterances.tsx
+++ src/components/posts/Utterances.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { Theme } from '../../constants';
+import useTheme from '../../hooks/useTheme';
 import themeStorage from '../../lib/storage/themeStorage';
 
 const SOURCE_URL = 'https://utteranc.es/client.js';
@@ -10,27 +11,46 @@ const DARK_THEME = 'github-dark';
 const LIGHT_THEME = 'github-light';
 const ISSUE_TERM = 'pathname';
 
+const MESSAGE_TARGET_ORIGIN = 'https://utteranc.es/';
+
+const getThemeByAppTheme = (theme: Theme | null) =>
+  theme === Theme.dark ? DARK_THEME : LIGHT_THEME;
+
 const Utterances = () => {
   const ref = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
     const utterances = document.createElement('script');
-    const config: { [key: string]: string } = {
+    const config: Record<string, string> = {
       src: SOURCE_URL,
       repo: REPO,
       branch: BRANCH,
-      theme: themeStorage.getTheme() === Theme.dark ? DARK_THEME : LIGHT_THEME,
+      theme: getThemeByAppTheme(themeStorage.getTheme()),
       label: LABEL,
       async: 'true',
       'issue-term': ISSUE_TERM,
     };
 
-    Object.keys(config).forEach((key) => {
-      utterances.setAttribute(key, config[key]);
+    Object.entries(config).forEach(([key, value]) => {
+      utterances.setAttribute(key, value);
     });
 
     ref.current?.appendChild(utterances);
   }, []);
+
+  useEffect(() => {
+    const utterancesFrame = document.querySelector<HTMLIFrameElement>(
+      'iframe.utterances-frame',
+    );
+
+    if (!utterancesFrame) return;
+
+    utterancesFrame.contentWindow?.postMessage(
+      { type: 'set-theme', theme: getThemeByAppTheme(theme) },
+      MESSAGE_TARGET_ORIGIN,
+    );
+  }, [theme]);
 
   return <div ref={ref} />;
 };
