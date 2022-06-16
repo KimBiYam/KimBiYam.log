dddca91299df4314fa10b64d23c6622ebbe61872
ChangHyunKim <hot9998@naver.com>
2022-06-16 23:56:36 +0900

    Migrate postPageAtom to jotai

--- src/hooks/useTag.ts
+++ src/hooks/useTag.ts
@@ -1,12 +1,12 @@
+import { useSetAtom } from 'jotai';
 import { NextRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
-import { useSetRecoilState } from 'recoil';
 import postPageAtom from '../atoms/postPageAtom';
 import { Tag } from '../constants';
 
 const useTag = (router: NextRouter) => {
   const [selectedTag, setSelectedTag] = useState<string>(Tag.all);
-  const setPostPage = useSetRecoilState(postPageAtom);
+  const setPostPage = useSetAtom(postPageAtom);
 
   const handleTagClick = useCallback((tag: string) => {
     const path = tag === Tag.all ? '/' : `/?tag=${tag}`;
