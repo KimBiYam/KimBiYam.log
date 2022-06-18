a9375f0f10cda75ede13edee099bd05e7a6f4eae
ChangHyunKim <hot9998@naver.com>
2022-06-19 03:41:36 +0900

    Add next-sitemap generateRobotsTxt option

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,5 +2,6 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL || 'https://kimbiyam.me',
   sitemapSize: 7000,
   changefreq: 'weekly',
+  generateRobotsTxt: true,
   priority: 1,
 };
