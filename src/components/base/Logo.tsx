f1a92bb3f71f64dc0aac9fbbc7daa8fc5d49c5d4
ChangHyunKim <hot9998@naver.com>
2022-06-20 12:29:35 +0900

    Change logo image size

--- src/components/base/Logo.tsx
+++ src/components/base/Logo.tsx
@@ -1,19 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { memo } from 'react';
-import favicon from '../../assets/favicon/favicon-32x32.png';
+import favicon from '../../assets/favicon/apple-icon-57x57.png';
 
 const Logo = () => (
   <Link href="/" passHref>
-    <a className="w-6 h-6">
-      <Image
-        priority
-        src={favicon}
-        alt="logo"
-        quality={100}
-        width={24}
-        height={24}
-      />
+    <a className="absolute w-6 h-6">
+      <Image priority src={favicon} alt="logo" layout="fill" quality={100} />
     </a>
   </Link>
 );
