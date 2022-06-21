6fcbf0d85d40ab1439eb05926736a10cfefe7056
ChangHyunKim <hot9998@naver.com>
2022-06-21 19:53:42 +0900

    Add background element into `TagSelector` component
    Fixed for post list item hover layout

--- src/components/posts/TagSelector.tsx
+++ src/components/posts/TagSelector.tsx
@@ -22,20 +22,25 @@ const TagSelector = forwardRef<HTMLDivElement, TagSelectorProps>(
 
     return (
       <div
-        className={`sticky z-40 flex pt-4 -mx-6 md:mx-0 overflow-auto scrollbar-hide main-container ${
+        className={`sticky z-40 ${
           direction === ScrollDirection.up ? 'top-14' : 'top-0'
         }`}
-        ref={ref}
       >
-        {tags.map((tag) => (
-          <TagButton
-            key={tag}
-            tag={tag}
-            onTagClick={onTagClick}
-            isSelected={tag === selectedTag}
-            ref={(instance) => registerChildRef(instance, tag)}
-          />
-        ))}
+        <div
+          className="flex pt-4 -mx-6 overflow-auto md:mx-0 scrollbar-hide main-container"
+          ref={ref}
+        >
+          {tags.map((tag) => (
+            <TagButton
+              key={tag}
+              tag={tag}
+              onTagClick={onTagClick}
+              isSelected={tag === selectedTag}
+              ref={(instance) => registerChildRef(instance, tag)}
+            />
+          ))}
+        </div>
+        <div className="absolute top-0 left-0 right-0 hidden h-full md:-mx-8 md:block -z-10 main-container" />
       </div>
     );
   },
