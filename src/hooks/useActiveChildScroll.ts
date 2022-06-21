f12d091e27cb929ae5994024cc80430f0b15429c
ChangHyunKim <hot9998@naver.com>
2022-06-21 19:26:56 +0900

    Change useActiveChildScroll hook's interface

--- src/hooks/useActiveChildScroll.ts
+++ src/hooks/useActiveChildScroll.ts
@@ -37,7 +37,7 @@ const useActiveChildScroll = <P extends HTMLElement, C extends HTMLElement>({
     [itemRefs],
   );
 
-  return { registerChildRef };
+  return registerChildRef;
 };
 
 export default useActiveChildScroll;
