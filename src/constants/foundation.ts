be9d131b4b128d5dda3eeb81814a2942b1fd75ec
ChangHyunKim <hot9998@naver.com>
2022-08-19 02:25:43 +0900

    Apply smooth scroll to table of conetns

--- src/constants/foundation.ts
+++ src/constants/foundation.ts
@@ -1,1 +1,2 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+export const IS_BROWSER = typeof window !== 'undefined';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import type { AppProps } from 'next/app';
 import smoothscroll from 'smoothscroll-polyfill';
-import { useEffect } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import Header from '../components/base/Header';
 import MainLayout from '../components/base/MainLayout';
 import useGoogleAnalyticsPageView from '../hooks/useGoogleAnalyticsPageView';
 import * as sentryUtil from '../lib/utils/sentry.util';
 import Footer from '../components/base/Footer';
-import { IS_PRODUCTION } from '../constants';
+import { IS_BROWSER, IS_PRODUCTION } from '../constants';
 import ScrollToTopButton from '../components/base/ScrollToTopButton';
 import { usePreserveScroll } from '../hooks/usePreserveScroll';
 
@@ -17,14 +16,11 @@ import '../lib/styles/tailwind.css';
 import PageHead from '../components/base/PageHead';
 
 if (IS_PRODUCTION) sentryUtil.init();
+if (IS_BROWSER) smoothscroll.polyfill();
 
 const App = ({ Component, pageProps }: AppProps) => {
   useGoogleAnalyticsPageView();
   usePreserveScroll();
-
-  useEffect(() => {
-    smoothscroll.polyfill();
-  }, []);
 
   return (
     <>
