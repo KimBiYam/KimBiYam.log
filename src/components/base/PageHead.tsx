90df7c8b1f8d6454a4a2d326338638d2ed698717
ChangHyunKim <hot9998@naver.com>
2022-08-21 11:59:11 +0900

    Relocate app head tags to main page

--- src/components/base/PageHead.tsx
+++ src/components/base/PageHead.tsx
@@ -6,8 +6,8 @@ import { DOMAIN_URL } from '../../constants';
 import Favicon from './Favicon';
 
 interface PageHeadProps {
+  title: string;
   description?: string;
-  title?: string;
   url?: string;
   ogImagePath?: string;
 }
@@ -22,8 +22,8 @@ const PageHead = ({
 }: PageHeadProps) => (
   <Head>
     <Favicon />
-    <title>{title ? title + PAGE_TITLE_SUFFIX : 'KimBiyam.log'}</title>
-    {url && <link rel="canonical" href={`${DOMAIN_URL}${url}`} />}
+    <title>{title + PAGE_TITLE_SUFFIX}</title>
+    <link rel="canonical" href={`${DOMAIN_URL}${url}`} />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover"
