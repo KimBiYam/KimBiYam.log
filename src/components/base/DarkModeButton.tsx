e188d7f56fc4e10f083b5088b8ae479cd08dae43
ChangHyunKim <hot9998@naver.com>
2022-08-22 21:29:38 +0900

    Relocate google analytics dark mode reports
    Relocated into dark mode button's event

--- src/components/base/DarkModeButton.tsx
+++ src/components/base/DarkModeButton.tsx
@@ -6,16 +6,23 @@ import MoonIcon from '../../assets/svgs/moon.svg';
 import SunIcon from '../../assets/svgs/sun.svg';
 import { Theme } from '../../constants';
 import useTheme from '../../hooks/useTheme';
+import * as googleAnalytics from '../../lib/googleAnalytics';
 import { createRotateScaleMotion } from '../../lib/styles/motions';
 
 const DarkModeButton = () => {
   const { theme, toggleTheme } = useTheme();
 
+  const handleClick = () => {
+    if (theme === Theme.dark) googleAnalytics.darkModeOff();
+    if (theme === Theme.light) googleAnalytics.darkModeOn();
+    toggleTheme();
+  };
+
   return (
     <button
       type="button"
       className="p-2 rounded-full cursor-pointer primary-button-hover"
-      onClick={toggleTheme}
+      onClick={handleClick}
       title="dark-mode-button"
     >
       <div className="relative w-6 h-6">
