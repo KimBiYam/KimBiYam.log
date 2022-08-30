bd5725502778ae154c89daed1e1422967330b9f3
ChangHyunKim <hot9998@naver.com>
2022-08-31 00:24:52 +0900

    Fix tag button's selected color

--- src/components/posts/TagButton.tsx
+++ src/components/posts/TagButton.tsx
@@ -9,8 +9,12 @@ interface TagButtonProps {
 const TagButton = forwardRef<HTMLButtonElement, TagButtonProps>(
   ({ tag, onTagClick, isSelected }, ref) => (
     <button
-      className={`px-4 py-1 mx-1 mb-2 rounded-xl text-xs md:text-sm whitespace-pre main-font-color primary-button primary-button-hover
-    ${isSelected ? 'font-bold' : 'font-semibold'}`}
+      className={`px-4 py-1 mx-1 mb-2 rounded-xl text-xs md:text-sm whitespace-pre main-font-color primary-button-hover
+    ${
+      isSelected
+        ? 'font-bold bg-slate-300 dark:bg-neutral-500'
+        : 'font-semibold bg-slate-200 dark:bg-neutral-700'
+    }`}
       ref={ref}
       type="button"
       onClick={() => onTagClick(tag)}
