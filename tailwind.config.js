2fb52699a737f3d9de9ed7f47df2e77c81a4ec8a
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-15 23:49:14 +0900

    Fix prose heading code tag's styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,6 +65,14 @@ module.exports = {
                 display: 'none',
               },
             },
+            'h1, h2, h3, h4, h5, h6': {
+              code: {
+                background: 'inherit !important',
+                color: 'inherit',
+                fontSize: 'inherit',
+                padding: 'inherit',
+              },
+            },
             img: {
               marginLeft: 'auto',
               marginRight: 'auto',
