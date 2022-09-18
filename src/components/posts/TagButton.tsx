98bc76c2dbce8493f9bca5bf297aa2f0a8a6b956
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-18 14:01:08 +0900

    Apply media-hover class to components

--- src/components/posts/TagButton.tsx
+++ src/components/posts/TagButton.tsx
@@ -9,7 +9,7 @@ interface TagButtonProps {
 const TagButton = forwardRef<HTMLButtonElement, TagButtonProps>(
   ({ tag, onTagClick, isSelected }, ref) => (
     <button
-      className={`px-4 py-1 mx-1 mb-2 text-xs whitespace-pre duration-300 rounded-xl md:text-sm transition-backgroundColor hover:bg-slate-300 dark:hover:bg-neutral-500 ${
+      className={`px-4 py-1 mx-1 mb-2 text-xs whitespace-pre duration-300 rounded-xl md:text-sm transition-backgroundColor media-hover:bg-slate-300 dark:media-hover:bg-neutral-500 ${
         isSelected
           ? 'font-bold bg-slate-300 dark:bg-neutral-500'
           : 'font-semibold text-neutral-400 bg-slate-200 dark:bg-neutral-700 dark:text-neutral-400'
