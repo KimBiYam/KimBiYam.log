e4a262e2196ae2ab645651810b482b61930cc161
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-23 01:18:03 +0900

    Add `Fira Code` font family web font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,10 @@ class MyDocument extends Document {
             as="style"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard-dynamic-subset.css"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap"
+            rel="stylesheet"
+          />
           <script src="/theme.js" />
           <script src="/setViewportProperty.js" />
         </Head>
