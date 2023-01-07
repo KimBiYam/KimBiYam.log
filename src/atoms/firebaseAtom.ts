5d7cbc7085eed78c7ff3f3291870dc2892075dcb
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-01-08 00:27:25 +0900

    Remove firebaseAppAtom

--- src/atoms/firebaseAtom.ts
+++ src/atoms/firebaseAtom.ts
@@ -1,7 +1,4 @@
 import { RemoteConfig } from '@firebase/remote-config';
-import { FirebaseApp } from 'firebase/app';
 import { atom } from 'jotai';
-
-export const firebaseAppAtom = atom<FirebaseApp | null>(null);
 
 export const remoteConfigAtom = atom<RemoteConfig | null>(null);
