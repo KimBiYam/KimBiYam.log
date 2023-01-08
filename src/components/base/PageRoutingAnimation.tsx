db95ebdbccd3e154ca04c885a9c1eecd1ee11abd
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-01-08 12:05:18 +0900

    Modify page routing animation's duration

--- src/components/base/PageRoutingAnimation.tsx
+++ src/components/base/PageRoutingAnimation.tsx
@@ -14,7 +14,7 @@ const PageRoutingAnimation = ({
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
-    transition={{ duration: 0.2, ease: 'easeInOut' }}
+    transition={{ duration: 0.25, ease: 'easeInOut' }}
   >
     {children}
   </m.div>
