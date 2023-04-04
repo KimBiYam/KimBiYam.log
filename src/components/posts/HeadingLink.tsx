106bb7f0c59fa969bade4e4dc1a94b20f2fe9436
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-04-05 00:17:43 +0900

    refactor: extract HeadingLink component

---
+++ src/components/posts/HeadingLink.tsx
@@ -0,0 +1,18 @@
+import LinkIcon from '../../assets/svgs/link_icon.svg';
+
+type HeadingLinkProps = {
+  href: string;
+  text: string | null;
+};
+
+const HeadingLink = ({ href, text }: HeadingLinkProps) => (
+  <a
+    href={href}
+    className="flex items-center no-underline hover:underline group"
+  >
+    {text}
+    <LinkIcon className="hidden w-4 h-4 ml-1 group-hover:block opacity-60" />
+  </a>
+);
+
+export default HeadingLink;
