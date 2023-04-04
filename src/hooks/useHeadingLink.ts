4d03a0264cafc7ca3c0a943af85df7fe7280e2e7
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-04-04 23:37:25 +0900

    refactor: change initialized to using useRef

--- src/hooks/useHeadingLink.ts
+++ src/hooks/useHeadingLink.ts
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const LINK_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 <path stroke-linecap="round" stroke-linejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
 </svg>`;
 
-let initialized = false;
-
 const useHeadingLink = (ref: React.RefObject<HTMLElement>) => {
+  const initializedRef = useRef(false);
+
   useEffect(() => {
-    if (initialized) return;
-    initialized = true;
+    if (initializedRef.current) return;
+    initializedRef.current = true;
 
     const originalHeadingElements: Map<Element, string> = new Map();
     const headingElements = ref.current?.querySelectorAll('h2,h3');
@@ -35,15 +35,10 @@ const useHeadingLink = (ref: React.RefObject<HTMLElement>) => {
       iconEl.classList.add('hidden', 'group-hover:block', 'opacity-60', 'ml-1');
       iconEl.innerHTML = LINK_ICON_SVG;
 
-      // eslint-disable-next-line no-param-reassign
       headingEl.innerHTML = '';
       anchorEl.appendChild(iconEl);
       headingEl.appendChild(anchorEl);
     });
-
-    return () => {
-      initialized = false;
-    };
   }, [ref]);
 };
 
