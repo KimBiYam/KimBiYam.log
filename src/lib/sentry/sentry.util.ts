2f96b66adb3fc6a73c377787e99853a7175b31da
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-04-05 01:19:03 +0900

    chore: add sentry replay options

--- src/lib/sentry/sentry.util.ts
+++ src/lib/sentry/sentry.util.ts
@@ -4,5 +4,8 @@ export const init = () => {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [new Sentry.Replay()],
   });
 };
