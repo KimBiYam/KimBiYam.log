d6fafe463c7897d561a9ef13d4818624fef56b22
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-05-24 20:55:17 +0900

    feat: remove header hide styles

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,31 +1,15 @@
 import { memo } from 'react';
 
 import { m } from 'framer-motion';
-import { useAtomValue } from 'jotai';
 
-import scrollAtom from '../../atoms/scrollAtom';
-import { ScrollDirection } from '../../constants';
-import { createDynamicallyOpacityMotion } from '../../lib/styles/motions';
 import HeaderButtonSection from './HeaderButtonSection';
 import HeaderDesktopNav from './HeaderDesktopNav';
 import Logo from './Logo';
 
-const VISIBLE_TRANSITION_MS = 100;
-const HEADER_HEIGHT = 32;
-
 const Header = () => {
-  const { direction, pageY, isRouting } = useAtomValue(scrollAtom);
-  const isShown = pageY < HEADER_HEIGHT / 2 || direction === ScrollDirection.up;
-
   return (
-    <m.header
-      className="fixed z-50 flex items-center w-full h-14 bg-inherit"
-      {...createDynamicallyOpacityMotion(
-        isShown,
-        isRouting ? 0 : VISIBLE_TRANSITION_MS,
-      )}
-    >
-      <div className="container flex items-center justify-between h-full px-4 max-w-screen-md md:px-8 ">
+    <m.header className="fixed z-50 flex items-center w-full h-14 bg-inherit">
+      <div className="container flex items-center justify-between h-full max-w-screen-md px-4 md:px-8 ">
         <Logo />
         <HeaderDesktopNav />
         <HeaderButtonSection />
