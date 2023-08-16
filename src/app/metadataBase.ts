036bc4ccb912ce8cc52d7f081129774dfd79881e
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-08-16 21:44:11 +0900

    refactor: remove unnecessary metadata codes

--- src/app/metadataBase.ts
+++ src/app/metadataBase.ts
@@ -3,16 +3,6 @@ import { Metadata } from 'next';
 import ogTagImage from '../assets/favicon/og_tag_image.png';
 import { DOMAIN_URL, PAGE_TITLE_SUFFIX } from '../constants';
 
-export const openGraphBase: Metadata['openGraph'] = {
-  type: 'website',
-  url: `${DOMAIN_URL}`,
-  images: {
-    url: DOMAIN_URL + ogTagImage,
-    width: 1200,
-    height: 630,
-  },
-};
-
 export const generateTitle = (title: string) => `${title}${PAGE_TITLE_SUFFIX}`;
 
 export const generateOpenGraphMetaData = ({
