b868d5038be6557ec0dba088843cf58cce8d7449
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-03-26 00:05:32 +0900

    feat: apply google font optimization

--- src/components/posts/MarkdownView.tsx
+++ src/components/posts/MarkdownView.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import { Fira_Code } from 'next/font/google';
+
 import rehypePrism from '@mapbox/rehype-prism';
 import rehypeSlug from 'rehype-slug';
 import rehypeStringify from 'rehype-stringify/lib';
@@ -10,6 +12,13 @@ import remarkGfm from 'remark-gfm';
 
 import useCreateHeadingLink from '@src/hooks/useCreateHeadingLink';
 import useMediumZoom from '@src/hooks/useMediumZoom';
+
+const firaCode = Fira_Code({
+  weight: '500',
+  preload: false,
+  display: 'swap',
+  variable: '--font-fira-code',
+});
 
 interface MarkdownViewProps {
   contentHtml: string;
@@ -34,6 +43,7 @@ export default function MarkdownView({ contentHtml }: MarkdownViewProps) {
   return (
     <div className="w-full max-w-full prose dark:prose-dark" ref={ref}>
       <ReactMarkdown
+        className={firaCode.variable}
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypePrism, rehypeSlug, rehypeStringify]}
       >
