eabcc4a0ef1955b2a4abe2c48535b9ea4b249d32
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-03-26 00:05:32 +0900

    feat: apply google font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,10 +56,6 @@ export default function RootLayout({ children }: PropsWithChildren<unknown>) {
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard-dynamic-subset.css"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap"
-          rel="stylesheet"
-        />
         <Script src="/setViewportProperty.js" />
         <Script src="/prettyConsole.js" strategy="lazyOnload" />
         <GoogleAnalytics />
