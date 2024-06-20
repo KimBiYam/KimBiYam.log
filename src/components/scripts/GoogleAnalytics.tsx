56b5462c7a555d6b0ecbcacfb5ad452e1ffc177d
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-06-20 23:11:14 +0900

    refactor: remove dangerouslySetInnerHTML

--- src/components/scripts/GoogleAnalytics.tsx
+++ src/components/scripts/GoogleAnalytics.tsx
@@ -8,20 +8,14 @@ const GoogleAnalytics = () => (
       strategy="afterInteractive"
       src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_TRACKING_ID}`}
     />
-    <Script
-      strategy="afterInteractive"
-      id="google-analytics"
-      dangerouslySetInnerHTML={{
-        __html: `
+    <Script strategy="afterInteractive" id="google-analytics">{`
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', '${GOOGLE_ANALYTICS_TRACKING_ID}', {
 page_path: window.location.pathname,
 });
-`,
-      }}
-    />
+`}</Script>
   </>
 );
 
