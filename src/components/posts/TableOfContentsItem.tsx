034076a37eef80533caea9a4b24df4dfd3682fac
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-06-22 22:54:09 +0900

    feat: add hover styles to TableOfContentsItem component

--- src/components/posts/TableOfContentsItem.tsx
+++ src/components/posts/TableOfContentsItem.tsx
@@ -21,7 +21,7 @@ const TableOfContentsItem = forwardRef<
 
   return (
     <motion.li
-      className="my-1 text-sm text-left"
+      className="text-sm"
       initial={{ x: -15, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 0.3, delay: level * 0.15 }}
@@ -31,7 +31,7 @@ const TableOfContentsItem = forwardRef<
         ref={ref}
         onClick={() => onClick(id)}
         className={clsx(
-          'text-left w-full overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer hover:opacity-100 transition-opacity duration-300',
+          'text-left w-full cursor-pointer hover:opacity-100 transition-opacity duration-300 btn-hover p-1 rounded-md',
           {
             'opacity-100 font-bold': activeId === id,
             'opacity-60': activeId !== id,
