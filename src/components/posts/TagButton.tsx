63b9174a6161067bf8d079f438d3858b76017280
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-06-22 22:53:51 +0900

    refactor: add .btn-hover tailwind utility class

--- src/components/posts/TagButton.tsx
+++ src/components/posts/TagButton.tsx
@@ -9,25 +9,25 @@ interface TagButtonProps {
 }
 
 const TagButton = forwardRef<HTMLButtonElement, TagButtonProps>(
-  ({ tag, onTagClick, isSelected }, ref) => (
-    <button
-      className={clsx(
-        'px-4 py-1 mx-1 mb-2 text-xs whitespace-pre duration-300 rounded-xl md:text-sm transition-backgroundColor hover:bg-slate-300 dark:hover:bg-neutral-500',
-        {
-          'font-bold bg-slate-300 dark:bg-neutral-500': isSelected,
-          'font-semibold text-neutral-400 bg-slate-200 dark:bg-neutral-700 dark:text-neutral-400':
-            !isSelected,
-        },
-      )}
-      ref={ref}
-      type="button"
-      onClick={() => onTagClick(tag)}
-    >
-      {tag.toUpperCase()}
-    </button>
-  ),
+  function TagButton({ isSelected, onTagClick, tag }, ref) {
+    return (
+      <button
+        className={clsx(
+          'px-4 py-1 mx-1 mb-2 text-xs whitespace-pre duration-300 rounded-xl md:text-sm transition-backgroundColor btn-hover',
+          {
+            'font-bold bg-slate-300 dark:bg-neutral-500': isSelected,
+            'font-semibold text-neutral-400 bg-slate-200 dark:bg-neutral-700 dark:text-neutral-400':
+              !isSelected,
+          },
+        )}
+        ref={ref}
+        type="button"
+        onClick={() => onTagClick(tag)}
+      >
+        {tag.toUpperCase()}
+      </button>
+    );
+  },
 );
-
-TagButton.displayName = 'TagButton';
 
 export default memo(TagButton);
