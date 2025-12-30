ef646dfbc1a11c8acd4eeffb18cbc0cb459eeef4
Chang Hyun Kim <ckdvk9998@gmail.com>
2025-12-30 12:50:56 +0900

    fix: fix theme flickering

--- public/initTheme.js
+++ public/initTheme.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-undef */
+const getTheme = () => {
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme');
+  }
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  }
+  return 'light';
+};
+
 const applyTheme = () => {
-  const theme = (() => {
-    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-      return localStorage.getItem('theme');
-    }
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    }
-    return 'light';
-  })();
+  const theme = getTheme();
 
   if (theme === 'dark') {
     document.documentElement.classList.add('dark');
@@ -19,4 +21,12 @@ const applyTheme = () => {
 
 applyTheme();
 
-document.addEventListener('astro:after-swap', applyTheme);
+document.addEventListener('astro:before-swap', (event) => {
+  const theme = getTheme();
+
+  if (theme === 'dark') {
+    event.newDocument.documentElement.classList.add('dark');
+  } else {
+    event.newDocument.documentElement.classList.remove('dark');
+  }
+});
